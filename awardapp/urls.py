b5bfc17c3d2c3dde9c357de1053859112d872bc5
omeddo <shizukane90@gmail.com>
2022-06-11 13:38:51 +0300

    added app

---
+++ awardapp/urls.py
@@ -0,0 +1,21 @@
+from django.urls import re_path as url , include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^$', views.index, name= 'index'),
+    url(r'^sites/$', views.sites, name= 'sites'),
+    url(r'^search/', views.search_projects, name='search_results'),
+    url(r'^project/(\d+)', views.get_project, name='project_results'),
+    url(r'^new/project$', views.new_project, name='new-project'),
+    url(r'^accounts/profile/$', views.user_profiles, name='profile'),
+    url(r'^ratings/', include('star_ratings.urls', namespace='ratings') ),
+    url(r'^api/projects/$', views.ProjectList.as_view()),
+    url(r'^api/profiles/$', views.ProfileList.as_view()),
+
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
