bd37712c1ceda5e40e552a95a1a57237050d6168
shizukane <drizydarolle@gmail.com>
2022-08-17 13:19:48 +0300

    made correction

--- awardproject/settings.py
+++ awardproject/settings.py
@@ -155,9 +155,9 @@ LOGIN_REDIRECT_URL = 'index'
 STAR_RATINGS_RANGE = 10
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT=  ''
+EMAIL_PORT=  587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'drizydarolle@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'boymsmartt@gmail.com'
+EMAIL_HOST_PASSWORD = 'nnloovqxlotjcilv'
 
 django_heroku.settings(locals())
